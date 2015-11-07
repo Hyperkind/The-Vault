e9a5dd52200c5801487af388d57645ab5e44890d
Jonathan AhNee <jonahnee@gmail.com>
2015-11-07 11:24:36 -1000

    all tests pass

--- vault.js
+++ vault.js
@@ -1,4 +1,20 @@
 'use strict';
-module.exports = function() {
-  
+module.exports = function () {
+
+  var secret = {};
+
+  return {
+
+    setValue : function ( key, value ) {
+        secret [ key ] = value;
+    },
+
+    getValue : function ( key ) {
+      if ( secret [ key ] ) {
+        return secret [ key ];
+      } else {
+        return null;
+      }
+    }
+  };
 };
